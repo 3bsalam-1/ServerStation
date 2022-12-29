5698a51498c03b98a89097519e26027a7289546c
ahmed mohamed <am5054936@gmail.com>
2022-12-29 19:48:13 +0200

    commit changes

---
+++ Blogs/Mazen_Ahmed/js/script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
---
+++ Blogs/Mohamed_Ashraf/README.md
@@ -0,0 +1,2 @@
+# personal-website
+simple personal website 
